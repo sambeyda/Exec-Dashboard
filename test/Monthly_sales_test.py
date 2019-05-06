952d513cd4c6ba85415403fc0578f1424597ccd5
Sam Beyda <sb1629@georgetown.edu>
2019-05-06 18:20:32 -0400

    Further USD proper formatting

--- test/Monthly_sales_test.py
+++ test/Monthly_sales_test.py
@@ -3,5 +3,11 @@
 from app.Monthly_sales import to_usd
 
 def test_to_usd():
-    #Tests whether number is returned with $ sign
-    assert to_usd(4.50) == "$4.50"
+    #Tests whether number is returned with $ sign and two decimal places
+    assert to_usd(10) == "$10.00"
+
+    #Tests whether number rounds decimal places
+    assert to_usd(10.3333) == "$10.33"
+
+    #Tests whether there is a thousand seperator
+    assert to_usd(10000) == "$10,000.00"
