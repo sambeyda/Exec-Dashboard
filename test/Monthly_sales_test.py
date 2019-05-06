7799f737f4e5e2553e10a3b4a4abfcd95126480e
Sam Beyda <sb1629@georgetown.edu>
2019-05-06 17:55:18 -0400

    Imported to_usd from app

--- test/Monthly_sales_test.py
+++ test/Monthly_sales_test.py
@@ -1,5 +1,7 @@
 #Testing for Executive Dashboard Project
 
+from app.Monthly_sales import to_usd
+
 def test_to_usd():
     #Tests whether number is returned with $ sign
     assert to_usd(4.50) == "$4.50"
